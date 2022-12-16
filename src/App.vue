4b21b2e4f9529f127662f3b74626ccf6307ac884
ylans <ylanss@yandex.ru>
2022-12-16 14:44:36 +0500

    init project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <section></section>
+</template>
+
+<script></script>
+
+<style lang="scss">
+#app {
+  min-height: 100vh;
+  padding: 0 15px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
